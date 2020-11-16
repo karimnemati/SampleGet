f9457e8e6824f91342501e8d158de59a376718f1
karimnemati <karim.nemati17@gmail.com>
2020-11-16 18:37:10 +0330

    Add Commit New

---
+++ SampleProject/Controllers/SliderController.cs
@@ -0,0 +1,15 @@
+namespace SampleProject.Controllers
+{
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class SliderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
